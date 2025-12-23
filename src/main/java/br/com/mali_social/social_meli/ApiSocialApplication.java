3c6529e40a9e43393484e4be9098026e9eb189e5
Cesarl99 <gomescesar987@gmail.com>
2025-12-23 16:46:46 -0300

    feat: adicionando ApiSocialApplication

---
+++ src/main/java/br/com/mali_social/social_meli/ApiSocialApplication.java
@@ -0,0 +1,14 @@
+package br.com.mali_social.social_meli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSocialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSocialApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/br/com/mali_social/social_meli/ApiSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mali_social.social_meli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSocialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
