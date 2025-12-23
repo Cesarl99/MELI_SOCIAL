5f71493ed720dc3166529bacbfde5fd60d7b7f58
Cesarl99 <gomescesar987@gmail.com>
2025-12-23 16:44:05 -0300

    feat(ListaUsuariosSeguindoresDto.java): adicionando chamada pra retorno da chamada a requisicao

---
+++ src/main/java/br/com/mali_social/social_meli/dto/ListaUsuariosSeguindoresDto.java
@@ -0,0 +1,39 @@
+package br.com.mali_social.social_meli.dto;
+
+import java.util.List;
+
+public class ListaUsuariosSeguindoresDto {
+    private long usuarioId;
+    private String nomeUsuario;
+    private List<UsuarioDto> seguidores;
+
+    public ListaUsuariosSeguindoresDto(long usuarioId, String nomeUsuario, List<UsuarioDto> seguidores) {
+        this.usuarioId = usuarioId;
+        this.nomeUsuario = nomeUsuario;
+        this.seguidores = seguidores;
+    }
+
+    public long getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(long usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public List<UsuarioDto> getSeguidores() {
+        return seguidores;
+    }
+
+    public void setSeguidores(List<UsuarioDto> seguidores) {
+        this.seguidores = seguidores;
+    }
+}
